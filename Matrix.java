6bd68af693df033f8bde0f7576acecd59c1e5a52
annabelleg <agary110@gmail.com>
2015-05-25 11:34:48 -0400

    row exchange, dilation, shearing for ref

--- Matrix.java
+++ Matrix.java
@@ -32,10 +32,26 @@ public class Matrix{
 	m.REF();
 System.out.println(m.displayMatrix());
     }
+
+    public void switchRows(int[] a, int[] b){
+	for (int c = 0; c < cols; c++){
+	    double temp = a[c];
+	    a[c] = b[c];
+	    b[c] = temp;
+	}
+    }
     
     public void REF(){
 	int leading1col = 0;
-	for (int r = 0; r < rows; r++){
+	int r = 0;
+	while (r < rows-1){
+	    if (data[r][0] == 0.0 && data[r+1][0] != 0.0){
+		switchRows(data[r], data[r+1]);
+	    }
+	    r++;
+	}
+	r=0;
+        while (r < rows){
 	    int c = 0;
 	    //DILATION
 	    while (c < cols && data[r][c] == 0.0){
@@ -48,6 +64,24 @@ System.out.println(m.displayMatrix());
 		    data[r][i] = data[r][i]*val;
 		}
 	    }
+	    //SHEARING
+	    int tempr = 0;
+	    while (tempr < rows){
+		if (tempr == r){
+		    tempr++;
+		}
+		if (data[tempr][leading1col] != 0.0){
+		    double shearval = data[r][leading1col];
+		    for (int i = 0; i < cols; i++){
+			data[tempr][i] = (-1)*shearval*(data[r][i]);
+		    }
+		}else if(data[tempr][leading1col] == 0.0){
+		    tempr++;
+		}
+		tempr++;
+	    }
+
+	    r++;
 	}
 		
     }
